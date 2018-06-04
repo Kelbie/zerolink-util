3b0c123286157227ce7ec77211d7a0e6d6068f5f
kevin <notkelbie@outlook.com>
2018-06-04 06:41:51 +0100

    added url variable

--- client.py
+++ client.py
@@ -23,20 +23,20 @@ class ZeroLink:
             'http':  'socks5h://localhost:9050',
             'https': 'socks5h://localhost:9050'
         }
-
+        self.url = "http://wtgjmaol3io5ijii.onion/api/v1/btc/ChaumianCoinJoin/"
         self.inputs = inputs
 
         self.states = None
         self.reference = None
 
     def getStates(self):
-        response = self.session.get('http://wtgjmaol3io5ijii.onion/api/v1/btc/ChaumianCoinJoin/states')
+        response = self.session.get(self.url + "states")
         response = json.dumps(json.loads(response.text), sort_keys=True, indent=4)
         self.states = ast.literal_eval(response)
         return self.states
 
     def postInputs(self):
-        response = self.session.post('http://wtgjmaol3io5ijii.onion/api/v1/btc/ChaumianCoinJoin/inputs', json=self.inputs)
+        response = self.session.post(self.url + "inputs", json=self.inputs)
         response = json.dumps(json.loads(response.text), sort_keys=True, indent=4)
         self.reference = ast.literal_eval(response)
         log("Post Input(s)")
@@ -45,7 +45,10 @@ class ZeroLink:
     @threaded
     def postConfirmation(self, loop=False):
         while True:
-            response = self.session.post('http://wtgjmaol3io5ijii.onion/api/v1/btc/ChaumianCoinJoin/confirmation?uniqueId={}&roundId={}'.format(self.reference["uniqueId"], self.reference["roundId"]))
+            response = self.session.post(
+                self.url + "confirmation?uniqueId={}&roundId={}".format(
+                    self.reference["uniqueId"],
+                    self.reference["roundId"]))
             log("Post Confirmation")
 
             if loop:
